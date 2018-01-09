92a4ce822186fc43299e7aca238855198f48e0e7
polyline <polyline@gapp.nthu.edu.tw>
2018-01-09 22:58:34 +0900

    Hello world

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-console.log('test')
 
+http = require('http');
+
+http.createServer(function(request, response){
+	//set HTTP header
+	//HTTP status: 200 //ok
+	//Content Type: text/plain
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.end('Hello, world!');
+}).listen(3000);
+
+console.log('successfully open server');
